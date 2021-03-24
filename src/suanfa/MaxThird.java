bcecf6a28d50bd4262583bd38f2199c72654a481
feng <fengqinfeng5@163.com>
2021-03-25 00:25:45 +0800

    heapSort extend

--- src/suanfa/MaxThird.java
+++ src/suanfa/MaxThird.java
@@ -1,43 +1,30 @@
 package suanfa;
 /*
-堆排  小顶堆  不稳定
+    利用堆来求  求第3大的数字   维护一个大小为3的最小堆
+    将堆顶元素和没加入堆得元素比较，如果比堆顶元素大，将堆顶和该元素交换，继续维护最小堆
+    最后答案第3大的数字就是堆顶元素
  */
-public class HeapSort {
+public class MaxThird {
     private static int n;
     private static int h[];
+    private static int number;
+    public MaxThird(){}
 
-    public HeapSort(){}
-
-    public HeapSort(int h[],int n){
+    public MaxThird(int h[],int n,int number){
         this.n=n;
         this.h=h;
-        create();
+        this.number=number;
+        create(number);
     }
     private void swap(int x,int y){
         int t=h[x];
         h[x]=h[y];
         h[y]=t;
     }
-
-    //向上调整
-    public void siftUp(int i){
-        int flag=0;
-        if(i==1)return ;
-        while(i!=1 && flag == 0){
-            if(h[i]<h[i/2]){
-                swap(i,i/2);
-            }else{
-                flag=1;
-            }
-            i=i/2;
-        }
-    }
-
-
-    //向下调整
+    //向下调整 维护一个最小堆
     private void siftDown(int i){
         int t,flag=0;
-        while(i*2 <= n && flag==0){
+        while(i*2 <= number && flag==0){
             //判断与左儿子大小
             if(h[i] > h[i*2]){
                 t=i*2;
@@ -45,7 +32,7 @@ public class HeapSort {
                 t=i;
             }
             //判断与右儿子大小
-            if(i*2+1 <= n){
+            if(i*2+1 <= number){
                 if(h[t] > h[i*2+1]){
                     t=i*2+1;
                 }
@@ -61,23 +48,19 @@ public class HeapSort {
         }
     }
     //创建堆   nlogn
-    private void create(){
-        for(int i=n/2;i>=1;i--){
+    private void create(int number){
+        for(int i=number;i>=1;i--){
             siftDown(i);
         }
     }
-    //取出堆顶元素并删除
-    private int deleteMax() {
-        int t = h[1];
-        h[1] = h[n];
-        n--;
-        siftDown(1);
-        return t;
-    }
-    //堆排序
-    public void heapSort(){
-        for(int i=1;i<=h.length-1;i++){
-            System.out.print(deleteMax()+" ");
+    public int heapSort(){
+        for(int i=4;i<=n;i++){
+            if(h[1]<h[i]){
+                swap(1,i);
+                siftDown(1);
+            }
         }
+        return h[1];
     }
+
 }
